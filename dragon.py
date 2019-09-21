dd33564efee6766fbc886ef858a14e468c977901
Jim Vaughan <jvaughan3@gmail.com>
2019-09-21 07:53:45 -0500

    Format code.

--- dragon.py
+++ dragon.py
@@ -1,12 +1,14 @@
 import random
 import time
 
+
 def displayIntro():
     print('''You are in a land full of dragons. In front of you,
 you see two caves. In one cave, the dragon is friendly
 and will share his treasure with you. The other dragon
 is greedy and hungry, and will eat you on sight.''')
     print()
+
 
 def chooseCave():
     cave = ''
@@ -15,6 +17,7 @@ def chooseCave():
         cave = input()
 
     return cave
+
 
 def checkCave(chosenCave):
     print('You approach the cave...')
@@ -31,6 +34,7 @@ def checkCave(chosenCave):
         print('Gives you his treasure!')
     else:
         print('Gobbles you down in one bite!')
+
 
 playAgain = 'yes'
 while playAgain == 'yes' or playAgain == 'y':
