c0ee78d29f8fba425d442d387a79603335544f31
Jim Vaughan <jvaughan3@gmail.com>
2019-10-13 18:49:18 -0500

    Display how many guesses
    Let the user know the maximum number of guesses they have to get the correct number.

--- guess.py
+++ guess.py
@@ -1,15 +1,16 @@
 # This is a Guess the Number game.
 import random
 
+MAX_GUESSES = 6
 guessesTaken = 0
 
 print('Hello! What is your name?')
 myName = input()
 
 number = random.randint(1, 20)
-print(f'Well, {myName}, I am thinking of a number between 1 and 20.')
+print(f'Well, {myName}, I am thinking of a number between 1 and 20. See if you can get it in {MAX_GUESSES} or fewer guesses.')
 
-for guessesTaken in range(6):
+for guessesTaken in range(MAX_GUESSES):
     print('Take a guess.')
     guess = input()
     guess = int(guess)
